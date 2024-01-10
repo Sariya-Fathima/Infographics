2e2ce50e1db7e47871eebb227d7f5ac62ae72f6d
Sariya-Fathima <153586540+Sariya-Fathima@users.noreply.github.com>
2024-01-10 11:54:01 +0000

    Update 22090314.py

--- 22090314.py
+++ 22090314.py
@@ -201,8 +201,8 @@ def load_save_and_display_image(input_path, output_path, dpi=300):
     plt.imshow(img)
     plt.show()
 
-input_path = "C:/Users/UMA/Downloads/22090314300.png"
-output_path = "22090314300_saved.png"
+input_path = "22090314300.png"
+output_path = "22090314300.png"
 load_save_and_display_image(input_path, output_path, dpi=300)
 
 
